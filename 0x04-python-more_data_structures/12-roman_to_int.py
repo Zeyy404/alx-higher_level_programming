0381625495ed6eda7355e62ca7c0aeeecf90595f
Zainab Salih <Zainab.Salih@outlook.com>
2023-12-06 17:55:55 +0000

    [TASKS 12{MODIFIED}]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    list_keys = [rom_num[x] for x in roman_string]
-    integer = 0
-    if roman_string:
-        for y in range(len(list_keys)):
-            integer += list_keys[y]
-            if list_keys[y] > list_keys[y - 1] and y != 0:
-                integer -= list_keys[y - 1] * 2
-        return integer
     if not isinstance(roman_string, str):
         return 0
-    if not roman_string:
+
+    r_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    integer = 0
+    if roman_string:
+        for i in range(len(roman_string)):
+            c_v = r_num[roman_string[i]]
+            n_v = r_num[roman_string[i + 1]] if i + 1 < len(roman_string) else 0
+
+            if c_v < n_v:
+                integer -= c_v
+            else:
+                integer += c_v
+
+        return integer
+    else:
         return 0
