05d113c8cd102e821474b8e1dead0abbde46fe34
Zainab Salih <Zainab.Salih@outlook.com>
2023-12-19 18:23:09 +0000

    [TASKS 6 {MODIFIED}]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -81,10 +81,10 @@ class Square:
         """
         if self.__size == 0:
             print()
-        else:
-            for i in range(0, self.__position[1]):
-                print("")
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
+            return
+        for i in range(0, self.__position[1]):
+            print("")
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
