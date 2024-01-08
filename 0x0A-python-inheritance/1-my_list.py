7e62c69d673cf0c172729c195df6b706d1cfab5d
Zainab Salih <Zainab.Salih@outlook.com>
2024-01-08 10:17:06 +0000

    [TASK 1]

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,11 +7,6 @@ Contains MyList class definition
 class MyList(list):
     """a subclass of the class list"""
 
-    def __init__(self):
-        """initializing the subclass"""
-
-        super().__init__()
-
     def print_sorted(self):
         """Prints the list, but sorted(ascending sort)"""
 
