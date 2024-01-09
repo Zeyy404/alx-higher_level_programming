2fc3ebc555313a9da3e94265079f1bdb237f2338
Zainab Salih <Zainab.Salih@outlook.com>
2024-01-09 16:31:59 +0000

    [TASK 7]

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     except FileNotFoundError:
         data = []
     data.extend(sys.argv[1:])
-    save_to_json_file(data, "add_items.json")
+    save_to_json_file(data, "add_item.json")
