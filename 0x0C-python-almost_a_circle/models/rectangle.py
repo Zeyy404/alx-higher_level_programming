687aabe6288bc57fabf7a956b5e151c1611c963f
Zainab Salih <Zainab.Salih@outlook.com>
2024-01-15 19:51:30 +0000

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,9 +109,9 @@ class Rectangle(Base):
            None
         """
         if not isinstance(value, int):
-            raise TypeError("x must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
