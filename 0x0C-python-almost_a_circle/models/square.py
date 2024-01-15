2802bcaf6af2450b562625879b22b8606f75976e
Zainab Salih <Zainab.Salih@outlook.com>
2024-01-15 20:23:26 +0000

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initailize the Square class"""
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Returns: string representation of the Square attributes"""
