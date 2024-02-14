ab30f427d01ba85c0a4af961814a891b1eae5a3e
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-14 02:10:00 +0000

    ALL TASKS

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- script that updates the score of Bob to 10 in the table second_table.
+-- by using Bob's name only
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- script that creates a table called first_table in the current database in your MySQL server.
+-- If the table first_table already exists, your script does not fail
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,
+       name VARCHAR(256)
+);
