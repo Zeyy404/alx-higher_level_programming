801d36203e8c93b336cb936417db0e615e3bed2f
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-15 23:51:29 +0000

    TASK 13{MODIFIED}

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows;
+ORDER BY number_of_shows DESC;
