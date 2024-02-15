a6944f735ad1a1e506a51cbd529b0daa15930a5d
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-15 22:24:17 +0000

    TASK 6{MODIFIED}

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-       id INT NOT NULL AUTO_INCRERMENT PRIMARY KEY,
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(256) NOT NULL
 );
