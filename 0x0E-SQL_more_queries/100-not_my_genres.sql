33b2de59b4fd3302b964ea9099bce525f09c4a94
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-16 00:11:53 +0000

    TASK 17{ADVANCED}

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT tv_genres.id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show.title ='Dexter';
+WHERE tv_shows.title ='Dexter';
 
 SELECT tv_genres.name
 FROM tv_genres
