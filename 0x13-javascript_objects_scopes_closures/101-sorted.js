0c8f222faaa59856dce3230f0aefddb8c1ff687b
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-12 23:56:40 +0000

    TASKS 11 & 12{ADVANCED}

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+const NewDict = {}
+
+for (const userId in dict) {
+    const occurance = dict[userId];
+    if (!(occurance in NewDict)) {
+	NewDict[occurance] = [];
+    }
+    NewDict[occurance].push(userId);
+}
+console.log(NewDict);
