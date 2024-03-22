62b3b42be3ad22361cea1c7fc4c290dc09a06cdb
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-22 05:33:56 +0000

    TASK 15{ADVANCED}

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,9 +15,10 @@ class State(Base):
        __tablename__ (str): The table name of the class
         id (int): The State id of the class
         name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
     """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete")
