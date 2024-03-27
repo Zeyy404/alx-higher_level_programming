18745de1c5894ef85418c42f65074ec6a1a438eb
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-27 23:37:55 +0000

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!//usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Return: A peak integer.
+    """
+    n = len(list_of_integers)
+
+    if n == 0:
+        return None
+
+    start = 0
+    end = n - 1
+
+    while start < end:
+        mid = (start + end) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            start = mid + 1
+        else:
+            end = mid
+
+    return list_of_integers[start]
