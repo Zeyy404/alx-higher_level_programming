1f5696086dca1f7e49e48cf57842a2c30d5d0e0b
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-27 23:03:39 +0000

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response=$(curl -s -w "%{size_download}" -o /dev/null "$1")
-echo "$response"
+# sends a request to that URL, and displays the size of the body of the response
+echo "$(curl -s -w "%{size_download}" -o /dev/null "$1")"
