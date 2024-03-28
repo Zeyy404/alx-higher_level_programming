2c058b25437f4dd58ee60edfc0f2cea466fd2c75
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-28 23:07:21 +0000

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,9 @@ import sys
 import urllib.request
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    request_id = response.headers.get('X-Request-Id')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        request_id = response.headers.get('X-Request-Id')
 
-print(request_id)
+    print(request_id)
