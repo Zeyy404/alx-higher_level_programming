d2772f97fb31285f1de2de6c8f5b1e65d8a19a3b
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-28 23:03:43 +0000

    Tasks 1 & 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,6 @@ import urllib.request
 
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
-    request_id =response.headers.get('X-Request-Id')
+    request_id = response.headers.get('X-Request-Id')
 
 print(request_id)
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import urllib.parse
 
 url = sys.argv[1]
 email = sys.argv[2]
-data = urllib.parse.urlencode({'email':email}).encode('utf-8')
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
 with urllib.request.urlopen(url, data) as response:
     body = response.read().decode('utf-8')
