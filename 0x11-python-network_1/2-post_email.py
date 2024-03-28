36dd87d291b09f0ca07093f3c86755cd20e1693a
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-28 23:08:49 +0000

    Task 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,11 +6,12 @@ import urllib.request
 import urllib.parse
 
 
-url = sys.argv[1]
-email = sys.argv[2]
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-with urllib.request.urlopen(url, data) as response:
-    body = response.read().decode('utf-8')
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
 
-print("Your email is:", email)
+    print("Your email is:", email)
