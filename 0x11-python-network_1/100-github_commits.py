5a0f53e411075279c1fa8a984303e80c45ac9ac8
Zainab Salih <Zainab.Salih@outlook.com>
2024-03-29 01:16:26 +0000

    Task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""takes 2 arguments in order to list 10 commits
+   (from the most recent to oldest) using the GitHub API"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = "https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+    response = requests.get(url)
+
+    commits = response.json()
+
+    for index in range(min(10, len(commits))):
+        commit = commits[index]
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
