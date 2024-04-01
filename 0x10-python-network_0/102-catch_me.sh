28abe7b059c7b8e50bbda5b442c5759dccda7ae4
Zainab Salih <Zainab.Salih@outlook.com>
2024-04-01 22:05:38 +0000

    Task 9{Advanced}

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to `0.0.0.0:5000/catch_me` that causes a respond with a message containing `You got me!`
-curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -L | grep -oP '(?<=<body>).*(?=</body>)'
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -L | awk -F'<body>|</body>' '{print $2}'
