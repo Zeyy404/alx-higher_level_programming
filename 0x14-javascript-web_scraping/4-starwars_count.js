b76c4950aa7eb4bb4b0bc4cbbe7d47928ad8bef8
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-22 00:08:49 +0000

    Task 4 - Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
-const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 request(apiUrl, (err, response, body) => {
-  if (!err) {
-    const films = JSON.parse(body).results;
-    const count = films.reduce((acc, film) => {
-      return acc + (film.characters.includes(wedgeAntillesUrl) ? 1 : 0);
-    }, 0);
-    console.log(count);
+  if (err) { console.error(err); } else if (response.statusCode !== 200) { console.error(response.statusCode); } else {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
