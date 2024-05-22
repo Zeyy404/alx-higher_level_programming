2dd36536d6d6b0e909f2161c63241444aba175d3
Zainab Salih <Zainab.Salih@outlook.com>
2024-05-22 01:14:16 +0000

    Task 7 - Characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,7 +5,7 @@ const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 request(apiUrl, (err, response, body) => {
   if (err) { console.error(err); } else if (response.statusCode !== 200) { console.error(response.statusCode); }
   const movieData = JSON.parse(body);
-  const characters = movieData.charcters;
+  const characters = movieData.characters;
   characters.forEach(characterUrl => {
     request(characterUrl, (err, response, body) => {
       if (err) { console.error(err); } else if (response.statusCode !== 200) { console.error(response.statusCode); }
